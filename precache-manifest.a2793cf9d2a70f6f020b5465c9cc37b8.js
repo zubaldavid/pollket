8de2e7af7662f61732f87f40873dd15f1ca966cc
David Zubal <zubaldavid@gmail.com>
2019-10-02 21:16:24 -0700

    Updates

---
+++ precache-manifest.a2793cf9d2a70f6f020b5465c9cc37b8.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "0ba13376183c465a68bb",
+    "url": "/static/js/main.0ba13376.chunk.js"
+  },
+  {
+    "revision": "bc05e52a8e06adf5a6b9",
+    "url": "/static/js/2.bc05e52a.chunk.js"
+  },
+  {
+    "revision": "81361c8d3b9a8ecffb3be3b2320aeec0",
+    "url": "/index.html"
+  }
+];
